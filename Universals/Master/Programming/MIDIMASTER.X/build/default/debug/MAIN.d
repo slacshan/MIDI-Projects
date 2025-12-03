40651f7f0f93560526691f8483f528bbe1bc037e
Payden Hoskins <paydenhoskins@gmail.com>
2025-12-03 16:18:09 -0700

    added the master code I have been working on.

---
+++ Universals/Master/Programming/MIDIMASTER.X/build/default/debug/MAIN.d
@@ -0,0 +1,2 @@
+build/default/debug\MAIN.i: MAIN.S 88Setup.inc I2C_Master.inc \
+  EUSART_SETUP.inc
---
+++ Universals/Master/Programming/MIDIMASTER.X/build/default/production/MAIN.d
@@ -0,0 +1,2 @@
+build/default/production\MAIN.i: MAIN.S 88Setup.inc I2C_Master.inc \
+  EUSART_SETUP.inc
